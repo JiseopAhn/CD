2935072fc26307a7bb08f4232f0765a3eebc8cbf
JiseopAhn <taste0607@kumoh.ac.kr>
2025-03-12 21:18:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 prediction_client = aiplatform.gapic.PredictionServiceClient()
 
 # 엔드포인트 구성: 실제 엔드포인트가 속한 프로젝트 ID 사용
-PROJECT_ID = "430202211120"
+PROJECT_ID = "learned-now-451904-n7"
 LOCATION = "asia-northeast3"
 ENDPOINT_ID = "5805856801261879296"
 ENDPOINT = f"projects/{PROJECT_ID}/locations/{LOCATION}/endpoints/{ENDPOINT_ID}"
